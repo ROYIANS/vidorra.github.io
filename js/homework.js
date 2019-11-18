9f700a5335ec78b88eb770df947f20cce03bbb51
Joshua <1294686101@qq.com>
2019-11-18 11:32:36 +0800

    submit

--- js/homework.js
+++ js/homework.js
@@ -1,16 +1,17 @@
 var link1 = "../note/" + DATE_NOW + ".html";
 var link2 = "../homework/" + DATE_PREV + ".html";
 var link3 = "../homework/" + DATE_NEXT + ".html";
-var link4 = "../OpenAsHTML.html?filename=posts/" + MD + ".md";
+var link4 = "../posts/" + MD + ".md";
+var link5 = "../OpenAsHTML.html?filename=posts/" + MD + ".md";
 
 
 document.title = "[Homework] - " + DATE_NOW + " - 张颖捷";
 document.getElementById("title").innerText = DATE_NOW;
 document.getElementById("description").innerText = DATE_NOW;
 document.getElementById("here").href = link1;
-document.getElementById("post").href = link4;
+document.getElementById("post").href = link5;
 document.getElementById("Article").innerText = link4;
-document.getElementById("prev").innerText = DATE_PREV;
+// document.getElementById("prev").innerText = DATE_PREV;
 document.getElementById("prev_link").href = link2;
 document.getElementById("next").innerText = DATE_NEXT;
 document.getElementById("next_link").href = link3;
